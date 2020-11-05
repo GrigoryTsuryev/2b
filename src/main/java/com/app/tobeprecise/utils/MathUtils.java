8f1387b2fd22db9a22e6d5a9350add6f9c42209b
Grigory <tsurievga@gmail.com>
2020-11-05 17:58:34 +0200

    assigment

--- src/main/java/com/app/tobeprecise/utils/MathUtils.java
+++ src/main/java/com/app/tobeprecise/utils/MathUtils.java
@@ -5,11 +5,8 @@ import java.util.Arrays;
 public class MathUtils {
 
     public static double calculateOverload(double[] array) {
-        double n= array.length;
-        double sum = Arrays.stream(array).sum();
-        double mean = sum / n;
-        double mean1 = Arrays.stream(array).map(el -> Math.pow((el - mean), 2)).sum() / (n - 1);
         double average = Arrays.stream(array).average().getAsDouble();
-        return Math.sqrt(mean1) + average;
+        double mean = Arrays.stream(array).map(el -> Math.pow((el - average), 2)).sum() / (array.length - 1);
+        return Math.sqrt(mean) + average;
     }
 }
