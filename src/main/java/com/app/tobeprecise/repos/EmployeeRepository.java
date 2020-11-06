f241920f36afc46d04ba5cd5a6b9fc64744c7e63
Grigory <tsurievga@gmail.com>
2020-11-06 09:45:49 +0200

    assigment

--- src/main/java/com/app/tobeprecise/repos/EmployeeRepository.java
+++ src/main/java/com/app/tobeprecise/repos/EmployeeRepository.java
@@ -3,8 +3,6 @@ package com.app.tobeprecise.repos;
 import com.app.tobeprecise.entities.Employee;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 
