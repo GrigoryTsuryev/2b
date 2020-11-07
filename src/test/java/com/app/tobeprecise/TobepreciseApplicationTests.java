206e988f94bbce0b82ecd38033f4178f92920024
Grigory <tsurievga@gmail.com>
2020-11-07 15:15:48 +0200

    assigment

--- src/test/java/com/app/tobeprecise/TobepreciseApplicationTests.java
+++ src/test/java/com/app/tobeprecise/TobepreciseApplicationTests.java
@@ -1,7 +1,5 @@
 package com.app.tobeprecise;
 
-import com.app.tobeprecise.entities.Employee;
-import com.app.tobeprecise.repos.EmployeeRepository;
 import com.app.tobeprecise.services.EmployeeServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.util.logging.Filter;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
